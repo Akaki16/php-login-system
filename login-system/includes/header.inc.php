2293beb22f403ee97fb770c0a274ad8c68be899e
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-06-10 06:44:13 -0700

    Add header

---
+++ login-system/includes/header.inc.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1) ?>
+
+<?php include_once 'config/pdo.php' ?>
+
+<?php include_once 'services/userService.php' ?>
+
+<?php include_once 'utils/functions.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login System</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <script src="../scripts/script.js" defer></script>
+</head>
+<body class="bg-dark">
